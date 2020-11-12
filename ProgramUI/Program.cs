8c282b6fa430c105d03f40e2de03ab1c4dd19ea7
Filip Milbredt <filip_milbredt@hotmail.com>
2020-11-12 11:13:44 +0100

    Hello Linn

--- ProgramUI/Program.cs
+++ ProgramUI/Program.cs
@@ -7,6 +7,7 @@ namespace ProgramUI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Filip!");
+            Console.WriteLine("Hello Linn!");
         }
     }
 }
