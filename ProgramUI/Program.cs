48bbe4652aac701052805bcfae04defac97cac3d
Filip Milbredt <filip_milbredt@hotmail.com>
2020-11-12 10:48:47 +0100

    Second commit

--- ProgramUI/Program.cs
+++ ProgramUI/Program.cs
@@ -6,7 +6,7 @@ namespace ProgramUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Filip!");
         }
     }
 }
