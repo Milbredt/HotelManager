2aa46673d099f93bc035c363ab2c44d79c49d11b
Filip Milbredt <filip_milbredt@hotmail.com>
2020-11-16 10:30:29 +0100

    User med subklasser skapad

---
+++ HotelClassLibrary/User.cs
@@ -0,0 +1,11 @@
+namespace HotelClassLibrary
+{
+    abstract class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
