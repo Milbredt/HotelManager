ca03d7edcfd664a1ff4469ad677c8e8bcb852702
95olibod <70889504+95olibod@users.noreply.github.com>
2020-11-16 15:59:34 +0100

    SISTA COMMITEN FÖR IDAG FRÅN OLLE OCH KOFFE

--- ProgramUI/Program.cs
+++ ProgramUI/Program.cs
@@ -2,43 +2,39 @@
 using System.Collections.Generic;
 using HotelClassLibrary;
 
-namespace ProgramUI
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ProgramUI {
+    class Program {
+        static void Main (string[] args) {
             string userName;
             string password;
 
-            HotelManager hotelManager = new HotelManager();
-            hotelManager.AddRoom();
-            UserAuthentication userAuthentication = new UserAuthentication();
-            userAuthentication.AddStaffUser("user", "pass", "firstname", "lastname");
+            HotelManager hotelManager = new HotelManager ();
+            hotelManager.AddRoom ();
+            UserAuthentication userAuthentication = new UserAuthentication ();
+            userAuthentication.AddStaffUser ("user", "pass", "firstname", "lastname");
 
-            System.Console.WriteLine("1- staff / 2- guest");
+            System.Console.WriteLine ("1- staff / 2- guest");
 
-            var input = Console.ReadKey();
+            var input = Console.ReadKey ();
 
-            switch (input.Key)
-            {
+            switch (input.Key) {
                 case ConsoleKey.D1: //Staff Login
-                    Console.WriteLine("Staff login");
-                    Console.Write("Username: ");
-                    userName = Console.ReadLine();
+                    Console.WriteLine ("Staff login");
+                    Console.Write ("Username: ");
+                    userName = Console.ReadLine ();
 
-                    userAuthentication.CheckIfUsernameExist(userName);
+                    userAuthentication.CheckIfUsernameExist (userName);
 
-                    Console.Write("Password: ");
-                    password = Console.ReadLine();
-                    userAuthentication.TryValidateStaffUser(userName, password);
-                    Console.WriteLine(userAuthentication.TryValidateStaffUser(userName, password));
-                    ChoiceForStaff();
+                    Console.Write ("Password: ");
+                    password = Console.ReadLine ();
+                    userAuthentication.TryValidateStaffUser (userName, password);
+                    Console.WriteLine (userAuthentication.TryValidateStaffUser (userName, password));
+                    ChoiceForStaff ();
 
                     break;
 
                 case ConsoleKey.D2: //Guest login
-                    ChoiceForGuest(hotelManager);
+                    ChoiceForGuest (hotelManager);
 
                     /*Console.WriteLine ("Guest login");
                     Console.Write ("Username: ");
@@ -55,73 +51,68 @@ namespace ProgramUI
             }
 
         }
-        static void ChoiceForGuest(HotelManager hotelManager)
-        {
-            Console.WriteLine("Guest");
-            Console.WriteLine("Make a choice below");
-            Console.WriteLine("1: View avaible rooms");
-            Console.Write("Choice: ");
+        static void ChoiceForGuest (HotelManager hotelManager) {
+            Console.WriteLine ("---------Book a room---------");
+                       Console.WriteLine (hotelManager.ViewAllRooms ());
 
-            var input = Console.ReadKey();
+            Console.WriteLine ("Wich room do you want to book?");
+            Console.Write("Roomnr: ");
 
-            switch (input.Key)
-            {
-                case ConsoleKey.D1:
-                    //se lediga rum
-                    string availableRooms = hotelManager.ViewAllRooms ();
+            var input = Console.ReadKey ();
 
-                    //string roomDescriptions = "";
+            /*                                  TA BORT?????????????????????????????!!!
+                        switch (input.Key) {
+                            case ConsoleKey.D1:
+                                //se lediga rum
 
-                    Console.WriteLine(availableRooms);
+                                //string roomDescriptions = "";
 
-                    // foreach (Room room in availableRooms) {
-                    //     roomDescriptions += "Number of beds: " + room.NumberOfBeds + ".\n" +
-                    //         "Price per night: " + room.PricePerNight + " SEK.\n" +
-                    //         "Square meters: " + room.SquareMeters + ".";
-                    // }
+                                // foreach (Room room in availableRooms) {
+                                //     roomDescriptions += "Number of beds: " + room.NumberOfBeds + ".\n" +
+                                //         "Price per night: " + room.PricePerNight + " SEK.\n" +
+                                //         "Square meters: " + room.SquareMeters + ".";
+                                // }
 
-                    break;
+                                break;
 
-                case ConsoleKey.D2:
-                    // se bokat rum
+                            case ConsoleKey.D2:
+                                // se bokat rum
 
-                    break;
+                                break;
 
-                default:
-                    break;
-            }
+                            default:
+                                break;*/
         }
+    
 
-        static void ChoiceForStaff()
-        {
-            System.Console.WriteLine("gör något av följande val....");
-            var input = Console.ReadKey();
+    static void ChoiceForStaff () {                         /// BYGG OM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        System.Console.WriteLine ("gör något av följande val....");
+        var input = Console.ReadKey ();
 
-            switch (input.Key)
-            {
-                case ConsoleKey.D1:
-                    // checkInGuest
-                    // MakeRoomUnavalible
-                    break;
+        switch (input.Key) {
+            case ConsoleKey.D1:
+                // checkInGuest
+                // MakeRoomUnavalible
+                break;
 
-                case ConsoleKey.D2:
-                    // checkOutGuest
-                    //payRoom
-                    // MakeRoomAvalible
-                    break;
+            case ConsoleKey.D2:
+                // checkOutGuest
+                //payRoom
+                // MakeRoomAvalible
+                break;
 
-                case ConsoleKey.D3:
-                    // BookRoom
-                    break;
+            case ConsoleKey.D3:
+                // BookRoom
+                break;
 
-                case ConsoleKey.D4:
-                    // Add Room
-                    break;
+            case ConsoleKey.D4:
+                // Add Room
+                break;
 
-                default:
-                    break;
-            }
+            default:
+                break;
         }
-
     }
+
+}
 }
