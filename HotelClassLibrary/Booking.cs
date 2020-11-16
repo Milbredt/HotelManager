18261861c3b7c5887fac425aa923b32cb356dc58
Filip Milbredt <filip_milbredt@hotmail.com>
2020-11-16 15:04:18 +0100

    Skapat Booking-klass

---
+++ HotelClassLibrary/Booking.cs
@@ -0,0 +1,18 @@
+namespace HotelClassLibrary
+{
+    class Booking
+    {
+        public int BookingId { get; set;}
+        public int RoomId { get; set;}
+        public int GuestId { get; set;}
+        public bool IsPaid { get; set;}
+
+        public Booking(int bookingId, int roomId, int guestId)
+        {
+            BookingId = bookingId;
+            RoomId = roomId;
+            GuestId = guestId;
+            IsPaid = false;
+        }
+    }
+}
