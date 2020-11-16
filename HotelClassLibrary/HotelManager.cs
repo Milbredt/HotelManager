da7d3eb6bd95f0474a0ecfd604a35cd273c6b5a0
Filip Milbredt <filip_milbredt@hotmail.com>
2020-11-16 10:11:17 +0100

    HotellManger skapad

---
+++ HotelClassLibrary/HotelManager.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace HotelClassLibrary
+{
+    class HotelManager
+    {
+        public Dictionary<int, Room> rooms = new Dictionary<int, Room>();
+        public Dictionary<int, int> staff = new Dictionary<int, int>();
+        public Dictionary<int, int> users = new Dictionary<int, int>();
+
+        
+
+
+
+
+    }
+}
