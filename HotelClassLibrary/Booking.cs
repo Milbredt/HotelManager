0ae0c6d6f05949236ff5633bef2641e53db6c1c0
Filip Milbredt <filip_milbredt@hotmail.com>
2020-11-18 13:10:47 +0100

    - extra rad

--- HotelClassLibrary/Booking.cs
+++ HotelClassLibrary/Booking.cs
@@ -2,6 +2,7 @@ namespace HotelClassLibrary
 {
     class Booking
     {
+        
         public int BookingId { get; set;}
         public int RoomId { get; set;}
         public int GuestId { get; set;}
