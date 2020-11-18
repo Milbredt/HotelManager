0b9d39571a6f94216e41c69c515e665ee86eeeec
Filip Milbredt <filip_milbredt@hotmail.com>
2020-11-18 15:16:53 +0100

    Ändrat RoomId till RoomNumber

--- HotelClassLibrary/Booking.cs
+++ HotelClassLibrary/Booking.cs
@@ -4,14 +4,14 @@ namespace HotelClassLibrary
     {
         
         public int BookingId { get; set;}
-        public int RoomId { get; set;}
+        public int RoomNumber { get; set;}
         public int GuestId { get; set;}
         public bool IsPaid { get; set;}
 
         public Booking(int bookingId, int roomId, int guestId)
         {
             BookingId = bookingId;
-            RoomId = roomId;
+            RoomNumber = roomId;
             GuestId = guestId;
             IsPaid = false;
         }
